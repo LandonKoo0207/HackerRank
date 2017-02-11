e00cb5cce7b53aa117ae9f6a62a9673e0e4e9b3c
LandonKoo0207 <landonkoo0207@gmail.com>
2017-02-11 22:30:16 +1100

    Comments added to the code.

--- algorithms/BalancedBrackets.py
+++ algorithms/BalancedBrackets.py
@@ -1,15 +1,25 @@
+## Soution for Balanced Bracket problem
+## Problem description at https://www.hackerrank.com/challenges/ctci-balanced-brackets/
+
 def isBalanced(exp):
+    # list of pairs of brackets
     pairs = ["{}", "[]", "()"]
     
+    # indicator whether the string has any pair
     pairExist = False
     
+    # for the given string(expression),
+    # find any pair of brackets, then remove the pair from the string. (used as the parameter for the recursion)
     for p in pairs:
         if p in exp:
             pairExist = True
             if len(exp) > 2:
                 idx = exp.find(p)
                 exp = exp[:idx] + exp[idx+2:]
-                       
+    
+    # if the given string doesn't have any pair, print NO
+    # else, if the string is 2 characters long and it's a pair, print YES
+    # otherwise, recursively call the function until the string meets the first 2 conditions. 
     if pairExist == False:
         print ("NO")
     elif len(exp) == 2 and exp in pairs:
