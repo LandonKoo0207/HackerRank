60cb2ed348428f7a88bc6e5e2125dc36293ffcb4
LandonKoo0207 <landonkoo0207@gmail.com>
2017-04-27 11:49:17 +1000

    Added comments

--- algorithms/CircularArrayRotation.py
+++ algorithms/CircularArrayRotation.py
@@ -1,3 +1,6 @@
+# Solution for Circular Array Rotation
+# Problem description at https://www.hackerrank.com/challenges/circular-array-rotation
+
 n,k,q = input().strip().split(' ')
 n,k,q = [int(n),int(k),int(q)]
 a = [int(a_temp) for a_temp in input().strip().split(' ')]
