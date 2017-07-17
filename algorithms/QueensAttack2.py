41302f6703475052403d5de0b75ee7af9c8b34dc
LandonKoo0207 <landonkoo0207@gmail.com>
2017-07-17 21:46:06 +1000

    Update QueensAttack2.py

--- algorithms/QueensAttack2.py
+++ algorithms/QueensAttack2.py
@@ -8,7 +8,7 @@ rQueen,cQueen = map(int, input().strip().split(' '))
 # attackable verticals(row and column)
 vertical = (n-1) * 2
 
-# attackable diaognals
+# attackable diagonals
 upper_left = min(n-rQueen, cQueen-1)
 bottom_right = min(rQueen-1, n-cQueen)
 upper_right = min(n-rQueen, n-cQueen)
@@ -21,12 +21,12 @@ col_right_obstacle = 0
 row_upper_obstacle = 0
 row_bottom_obstacle = 0
 
-# obstacles in the diaognal to the queen
+# obstacles in the diagonal to the queen
 # The last element in a tuple indicates whether there is any obstacle in the diagonal direction
-diaognal_upper_left = (n,1, False)
-diaognal_bottom_right = (1,n, False)
-diaognal_upper_right = (n,n, False)
-diaognal_bottom_left = (1,1, False)
+diagonal_upper_left = (n,1, False)
+diagonal_bottom_right = (1,n, False)
+diagonal_upper_right = (n,n, False)
+diagonal_bottom_left = (1,1, False)
 
 # Count the attackable squares only when there is any obstacle
 # Otherwise print all the attackable squares
@@ -55,44 +55,44 @@ if k != 0:
                 if n - rObstacle + 1 > row_upper_obstacle:
                     row_upper_obstacle = n - rObstacle + 1
                     
-        # For the obstacle is in the diaognal to the queen
+        # For the obstacle is in the diagonal to the queen
         else:
             # if the obstacle is not affecting the attackable squares
             # skip the counting
             if abs(rQueen - rObstacle) == abs(cQueen - cObstacle):
                 if rObstacle - rQueen > 0:
-                    # For obstacles in the diaognal direction, located to the upper of the queen
+                    # For obstacles in the diagonal direction, located to the upper of the queen
                     if cObstacle < cQueen:
-                        if cObstacle > diaognal_upper_left[1]:
-                            diaognal_upper_left = (rObstacle, cObstacle, True)
+                        if cObstacle > diagonal_upper_left[1]:
+                            diagonal_upper_left = (rObstacle, cObstacle, True)
                     elif cObstacle > cQueen:
-                        if cObstacle < diaognal_upper_right[1]:
-                            diaognal_upper_right = (rObstacle, cObstacle, True)
+                        if cObstacle < diagonal_upper_right[1]:
+                            diagonal_upper_right = (rObstacle, cObstacle, True)
                 elif rObstacle - rQueen < 0:
-                    # For obstacles in the diaognal direction, located to the lower of the queen
+                    # For obstacles in the diagonal direction, located to the lower of the queen
                     if cObstacle < cQueen:
-                        if cObstacle > diaognal_bottom_left[1]:
-                             diaognal_bottom_left = (rObstacle, cObstacle, True)
+                        if cObstacle > diagonal_bottom_left[1]:
+                             diagonal_bottom_left = (rObstacle, cObstacle, True)
                     elif cObstacle > cQueen:
-                        if cObstacle < diaognal_bottom_right[1]:
-                            diaognal_bottom_right = (rObstacle, cObstacle, True)
+                        if cObstacle < diagonal_bottom_right[1]:
+                            diagonal_bottom_right = (rObstacle, cObstacle, True)
 
     # Count the attackable squares in the vertical directions (row and column)
     vertical -= col_left_obstacle + col_right_obstacle + row_bottom_obstacle + row_upper_obstacle
 
-    # Count the attackable squares in the diaognal directions
+    # Count the attackable squares in the diagonal directions
     # Reduce the count only when there is any obstacle in a dianognal direction
     # otherwise, count all the possible squares in the direction
-    if diaognal_upper_left[2]:
-        upper_left -= min(n - diaognal_upper_left[0] + 1, diaognal_upper_left[1])
+    if diagonal_upper_left[2]:
+        upper_left -= min(n - diagonal_upper_left[0] + 1, diagonal_upper_left[1])
 
-    if diaognal_bottom_right[2]:
-        bottom_right -= min(diaognal_bottom_right[0], n - diaognal_bottom_right[1] + 1)
+    if diagonal_bottom_right[2]:
+        bottom_right -= min(diagonal_bottom_right[0], n - diagonal_bottom_right[1] + 1)
 
-    if diaognal_upper_right[2]:
-        upper_right -= min(n - diaognal_upper_right[0] + 1, n - diaognal_upper_right[1] + 1)
+    if diagonal_upper_right[2]:
+        upper_right -= min(n - diagonal_upper_right[0] + 1, n - diagonal_upper_right[1] + 1)
 
-    if diaognal_bottom_left[2]:
-        bottom_left -= min(diaognal_bottom_left[0], diaognal_bottom_left[1])
+    if diagonal_bottom_left[2]:
+        bottom_left -= min(diagonal_bottom_left[0], diagonal_bottom_left[1])
 
 print (vertical + upper_left + bottom_right + upper_right + bottom_left)
